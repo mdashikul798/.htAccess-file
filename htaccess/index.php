a1b5bb8db7f440b7b619217ab0a1633079402f0d
mdashikul798 <mdashikul798@gmail.com>
2020-07-10 21:08:54 +0600

    .htAccess file with many restriction

---
+++ htaccess/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<a href="home.php">Click me</a>
+	<h3>Hellow World, I am form index.php File</h3>
+</body>
+</html>
